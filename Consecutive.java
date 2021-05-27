f3401736a89730f5326350c4f832e255559e3cc4
Jolly Ray <65182142+rayjolly@users.noreply.github.com>
2021-05-27 11:48:15 +0530

    Add files via upload

---
+++ Consecutive.java
@@ -0,0 +1,20 @@
+public class Consecutive    
+{
+	static int[] a= {1,0,0,0,0,1,0,0,0,1};
+    
+    public static void main(String[] args) {
+        int sum = 0;
+        int max  = 0 ;
+        for(int i = 0 ; i< a.length ; i++){
+           if(a[i] == 0){
+               sum = 0;
+           } else {
+               sum++;
+               if(sum > max)
+                   max = sum;
+           }
+        }
+        
+        System.out.println("Max consecutive 1s is: "+max);
+    }
+}
